f250a44a6ce4d0dd6932e4c3e8dfe33eb80075bf
Chris Bongers <chrisbongers@gmail.com>
2021-11-27 16:13:01 +0400

    Added tailwind and supabase auth

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import CountryList from '../components/countryList';
+import { useEffect, useState } from 'react';
+import { supabase } from '../lib/initSupabase';
+import Login from '../components/Login';
+import Profile from '../components/Profile';
 
 export default function Home() {
-  return (
-    <main>
-      <CountryList />
-    </main>
-  );
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  return <main>{!session ? <Login /> : <Profile session={session} />}</main>;
 }
